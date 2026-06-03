7c39cb11cf1468b80e17f583c794a07077cf5956
kvr-007 <venkat.apl007@gmail.com>
2026-06-03 17:10:56 -0500

    conversations script added

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo"Rahim:: hi Robert"
-echo"Robert::Hello Rahim"
-echo"Rahim:: How are you doing?"
-echo"Robert::I am good. How are you?"
+echo "Rahim:: hi Robert"
+echo "Robert::Hello Rahim"
+echo "Rahim:: How are you doing?"
+echo "Robert::I am good. How are you?"
