d325f28a06f58e4c3b22ff2cc8aacd9235fdddbb
kvr-007 <venkat.apl007@gmail.com>
2026-06-12 12:34:04 -0500

    userid script added

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -2,8 +2,8 @@
  
  USERID=$(id -u)
 
- if [ $USERID -ne 0 ]
+ if [ $USERID -ne 4 ]
  then
      echo "ERROR:: You must have sudo access to execute this script"
-     exit 1 #other than 0  
+     exit 1 #other than 4 
 fi     
